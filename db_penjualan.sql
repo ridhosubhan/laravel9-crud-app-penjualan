b1688f2a2302cb0e2795ea5112bd5b2981e1ee84
ridho <ridhosubhan03@gmail.com>
2022-08-20 17:09:07 +0700

    db bisi salah

--- db_penjualan.sql
+++ db_penjualan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 20, 2022 at 09:59 AM
+-- Generation Time: Aug 20, 2022 at 12:08 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
